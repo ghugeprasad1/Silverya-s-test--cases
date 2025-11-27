9b8e077329efc938b1389df003c38ba33871a002
SilveyaRexee_a3qfdl8 <SilveyaRexee_a3qfdl8@BPKLAP-EI0324>
2025-11-28 02:16:23 +0530

    Save

---
+++ Object Repository/Login/patient_CAPTCHA.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>patient_CAPTCHA</name>
+   <tag></tag>
+   <elementGuidId>8ba7d398-9f96-4724-959e-9255e86af41e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[@role='checkbox' and @id='recaptcha-anchor']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
