f0df74ec7ae6239e6dc3873c709fc40e68ec89a0
SilveyaRexee_a3qfdl8 <SilveyaRexee_a3qfdl8@BPKLAP-EI0324>
2025-12-02 10:01:23 +0530

    Save locator change for supporting docs page

---
+++ Object Repository/SupportingDocumentsPage/documentType_PA_DriversLicensePhotoID.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>documentType_PA_DriversLicensePhotoID</name>
+   <tag></tag>
+   <elementGuidId>64b21482-4fe9-444e-acd7-51e69fc5d57a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//p[normalize-space(text())=&quot;Driver's License / Photo ID&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
