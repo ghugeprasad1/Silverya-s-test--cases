63696d20a73e990ccc822f3cdf0b227708b2cbea
SilveyaRexee_a3qfdl8 <SilveyaRexee_a3qfdl8@BPKLAP-EI0324>
2025-12-02 17:43:57 +0530

    Fix the failed test case

--- Scripts/MCC/EditAndSave/Validate Portal Patient Window Account Edit Load Page default to last page/Patient_Verify edit opens first page when Patient Form is complete/Script1764077082209.groovy
+++ Scripts/MCC/EditAndSave/Validate Portal Patient Window Account Edit Load Page default to last page/Patient_Verify edit opens first page when Patient Form is complete/Script1764077082209.groovy
@@ -22,7 +22,7 @@ try {
 
     WebUI.delay(10)
 
-    WebUI.callTestCase(findTestCase('MCC/General/Login_ParkViewHospital'), [:], FailureHandling.STOP_ON_FAILURE)
+    WebUI.callTestCase(findTestCase('MCC/General/PA_Login'), [:], FailureHandling.STOP_ON_FAILURE)
 
     WebUI.click(findTestObject('Page_MyCareCoverage/PatientTab'))
 
