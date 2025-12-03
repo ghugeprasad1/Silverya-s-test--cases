ec3466f7dfa52ef6f0a95d403dff1f355a67f0ae
SilveyaRexee_a3qfdl8 <SilveyaRexee_a3qfdl8@BPKLAP-EI0324>
2025-12-03 20:12:47 +0530

    Save

--- Object Repository/SubmittedConfirmationPage/InsuranceProductValue_PatientSummary.rs
+++ Object Repository/SubmittedConfirmationPage/InsuranceProductValue_PatientSummary.rs
@@ -1,16 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>Field_PatientSummary_InsuranceProduct</name>
+   <name>InsuranceProductValue_PatientSummary</name>
    <tag></tag>
-   <elementGuidId>804512c7-dedd-4aa5-89b7-339c0de82196</elementGuidId>
+   <elementGuidId>be74dd8b-af44-4328-ac5b-c6db1285c434</elementGuidId>
    <selectorCollection>
       <entry>
          <key>XPATH</key>
-         <value>//p[normalize-space(text())='Insurance Product:']</value>
+         <value>//p[text()='Insurance Product:']/following-sibling::p</value>
       </entry>
       <entry>
          <key>BASIC</key>
+         <value></value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
